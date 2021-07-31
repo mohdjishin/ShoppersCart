e45a22244dc015577ce4b62cdab0afc0773bc3ff
MUHAMMED JISHIN <jishinjamaltcp@gmail.com>
2021-07-31 19:27:34 +0530

    session

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var hbs = require("express-handlebars")
 var fileUpload = require("express-fileupload")
 var db = require("./config/connection")
+var  session = require("express-session")
 
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
@@ -22,7 +23,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use(session({secret:"Key",cookie:{maxAge:600000}})) 
+//max age is for time
 app.use(fileUpload())
 
 db.connect((err) => {
--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,8 @@ const userHelpers = require("../helpers/user-helpers")
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  let user=req.session.user
+  console.log(user)
 
 
 
@@ -14,8 +16,8 @@ router.get('/', function (req, res, next) {
 
 
   productHelpers.getAllProducts().then((products) => {
-    console.log(products)
-    res.render('user/view-products', { products })
+    
+    res.render('user/view-products', { products,user })
 
   })
 });
@@ -38,11 +40,17 @@ router.post("/login",(req,res)=>{
 userHelpers.doLogin(req.body).then((response)=>{
 
   if(response.status){
+    req.session.loggedIn=true
+    req.session.user=response.user
     res.redirect("/")
   }else{
     res.redirect("/login")
   }
 })
 
+})
+router.get("/logout",(req,res)=>{
+  req.session.destroy() //used to clear session
+  res.redirect("/")
 })
 module.exports = router;
